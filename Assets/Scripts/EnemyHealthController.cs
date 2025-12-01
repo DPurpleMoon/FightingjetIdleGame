d84b685885d4fd392c9727bb4811fc3b28b5eb2f
SzeZhng <szezhnglee@gmail.com>
2025-12-01 09:22:13 +0800

    Implemented health reduce mechanic

--- Assets/Scripts/EnemyHealthController.cs
+++ Assets/Scripts/EnemyHealthController.cs
@@ -1,27 +1,56 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class EnemyHealthController : MonoBehaviour
 {
-    public Slider healthSlider;
-    public float maxDisplayHealth = 100f;
+    private Slider healthSlider;
+    private float maxDisplayHealth = 100f;
     private float currentDisplayHealth;
+    private float MaxHealth;
     public EnemyData Data;
     private float currentHealth;
+    public EnemySpawnController Controller;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {   
+        Controller = GetComponent<EnemySpawnController>();
+        List<Slider> HealthBarList = Controller.DupeEnemyHealthList;
+        foreach (Slider HealthBar in HealthBarList)
+        {
+            if (HealthBar != null && HealthBar.name == $"{gameObject.name}HPBar")
+                { 
+                    healthSlider = HealthBar;
+                }
+        }
+        MaxHealth = Data.maxHealth;
         healthSlider.maxValue = maxDisplayHealth;
-        currentHealth = Data.maxHealth;
-        currentDisplayHealth = currentHealth * maxDisplayHealth / Data.maxHealth;
+        currentHealth = MaxHealth;
+        currentDisplayHealth = currentHealth * maxDisplayHealth / MaxHealth;
         healthSlider.value = currentDisplayHealth;
     }
 
-    public void HealthBarChange() {
-        
-    }
-    // Update is called once per frame
-    void Update()
+    // health reduce demo
+    void OnMouseDown()
     {
-        
+        HealthBarChange(5);
+    }
+
+    public void HealthBarChange(int hpReduced) {
+        currentHealth -= hpReduced;
+        currentDisplayHealth = currentHealth * maxDisplayHealth / MaxHealth;
+        healthSlider.value = currentDisplayHealth;
+        if (currentHealth <= 0)
+        {
+            if (Controller.DupeEnemyHealthList.Contains(healthSlider))
+            {
+                Controller.DupeEnemyHealthList.Remove(healthSlider);
+            }
+            if (Controller.DupeEnemyList.Contains(gameObject))
+            {
+                Controller.DupeEnemyList.Remove(gameObject);
+            }
+            Destroy(healthSlider);
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/StageList.cs
+++ Assets/Scripts/StageList.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class StageList : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StagePressed()
     {
         SceneManager.LoadScene("Stage0");
