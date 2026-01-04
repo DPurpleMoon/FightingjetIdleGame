c61033e70a5d6476cded37eba85abb0ab9c45bb8
SzeZhng <szezhnglee@gmail.com>
2026-01-05 02:44:51 +0800

    create file reading for stagelist

--- Assets/Scripts/StageList.cs
+++ Assets/Scripts/StageList.cs
@@ -1,8 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class StageList : MonoBehaviour
 {
+    void Start()
+    {
+        string streamingpath = Application.streamingAssetsPath;
+        string[] filePaths = Directory.GetFiles($"{streamingpath}/levellist/", "*.json", SearchOption.AllDirectories);
+        foreach (string filepath in filePaths)
+        {
+            
+        string fileName = Path.GetFileName(filepath);
+        int index = fileName.IndexOf(".json");
+        if (index >= 0)
+            {
+                fileName = fileName.Substring(0, index);
+            }
+        
+        int stagenum = int.Parse(fileName.Substring(5));    
+        Debug.Log(stagenum);
+        }
+    }
     public void StagePressed()
     {
         SceneManager.LoadScene("Stage0");
